ac9f4cf9820a487d5b58918047eb0149b3d25874
Paeng <gwpaeng@naver.com>
2020-07-18 09:04:26 +0000

    scraping success

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scrapper import get_jobs
 
 app = Flask("Job_Scrapper")
 
+db = {}#계속 찾으면 시간이 많이 걸리기 때문에 fake db를 만들어 준다.
 
 @app.route("/")
 #"/"로 접속요청을 하면 파이썬 함수(home)로 실행
@@ -16,12 +17,17 @@ def report():
   word = request.args.get("word")
   if word: #글씨 없는 것 check
     word = word.lower() #모든 글자 소문자로 만들어주기
-    jobs = get_jobs(word) #web_site url에서 오는 word를 받는다.
+    existingJobs = db.get(word) #db에서 먼저 찾아본다.
+    if existingJobs: 
+      jobs = existingJobs
+    else:
+      jobs = get_jobs(word) #web_site url에서 오는 word를 받는다.
+      db[word] = jobs
   else:
     return redirect("/") #아무 것도 없을 시 원래로 돌려주기(report)
   #report.html로 word데이터 넘겨주기
   #report.html의 {{searchingBy}} 에 word를 넣어준다.
-  return render_template("report.html",searchingBy=word)
+  return render_template("report.html", searchingBy=word, resultsNumber = len(jobs), jobs = jobs) #jobs = jobs는 렌더링 하는 것이다.(모두 보내주는 일)
 
 
 # @app.route("/<username>")
