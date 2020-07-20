1cd8f8ce9a4beb4973ceb538f12dae45a13ff7d1
Paeng <gwpaeng@naver.com>
2020-07-20 12:30:26 +0000

    Done

--- exporter.py
+++ exporter.py
@@ -3,7 +3,7 @@ import csv
 def save_to_file(jobs):
   file = open("jobs.csv", mode="w")# 쓰기 용 file 만들기 
   writer = csv.writer(file)
-  writer.writerow(["title", "company", "location", "link"])
+  writer.writerow(["Title", "Company", "Location", "Link"])
   
   for job in jobs:
     #title,company,location,link가 모여있는 곳에서 제목빼고 내용만 저장하는 방법
